4fa34394c93ce6d5927f3c8409025334cd7dbd2c
Elis <199841193+Elis-2@users.noreply.github.com>
2025-02-19 16:51:14 +0100

    completed_scan_plus_comm_protocol_implement

--- main.ts
+++ main.ts
@@ -7,29 +7,54 @@ let capture = false
 let positioning = false
 let approach = false
 let turn = false
+let turn_right = false
+let turning_right = false
+let turn_left = false
+let turning_left = false
 let turning = false
+radio.setGroup(2)
+radio.sendString("")
+basic.showIcon(IconNames.No)
 maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
 maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 scan = true
 basic.forever(function () {
-    if (turn && !turning) {
+    if (turn_right && !turning_right) {
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        turning_right = true
+        turning_left = false
+        turning = true
+    }else if (turn_left && !turning_left) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        turning_left = true
+        turning_right = false
         turning = true
     } else if (!turn && turning) {
         maqueen.motorStop(maqueen.Motors.All)
         turning = false
+        turning_left = false
+        turning_right = false
     }
 })
 basic.forever(function () {
     if (scan) {
         dist = maqueen.Ultrasonic()
         turn = true
+        turn_right = true
         if (dist <= 40) {
             turn = false
+            turn_right = false
             scan = false
+            approach = true
         }
         basic.pause(25)
-
+    }
+})
+radio.onReceivedNumber(function(receivedNumber: number) {
+    if (receivedNumber == 0){
+        basic.showIcon(IconNames.Yes)
+        radio.sendNumber(1)
     }
 })
