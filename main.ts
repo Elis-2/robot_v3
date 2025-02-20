f805ad8f47b0d0b4a65a4cfa36c0ef962e97ae76
Elis <199841193+Elis-2@users.noreply.github.com>
2025-02-20 09:53:40 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -14,6 +14,10 @@ radio.onReceivedNumber(function (receivedNumber) {
     }else if (receivedNumber == 2){
         basic.showIcon(IconNames.Target)
         scan = true
+    }else if (receivedNumber == 3){
+        scan = false
+        approach = false
+        move = false
     }
 })
 function fnc_turn_left () {
@@ -67,15 +71,21 @@ radio.onReceivedValue(function (name, value) {
         turn_servo()
     }
 })
+let movement_speed = 25
+let turn_speed = 25
 let scan = false
 let approach = false
 let dist = 0
-let turning = false
-let turn = false
+let moving = false
+let move = false
 let turning_left = false
 let turning_right = false
 let turn_right = false
 let turn_left = false
+let move_forward = false
+let move_back = false
+let moving_forward = false
+let moving_back = false
 let pos_servo = 0
 let positioning = false
 let capture = false
@@ -89,20 +99,24 @@ maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
 maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
 basic.forever(function () {
     if (turn_right && !(turning_right)) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 25)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, turn_speed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, turn_speed)
         turning_right = true
         turning_left = false
-        turning = true
+        moving = true
     } else if (turn_left && !(turning_left)) {
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
-        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 25)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, turn_speed)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, turn_speed)
         turning_left = true
         turning_right = false
-        turning = true
-    } else if (!(turn) && turning) {
+        moving = true
+    }else if (move_forward && !moving_forward){
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW,movement_speed)
+    }else if (move_back && !moving_back){
+        maqueen.motorRun(maqueen.Motors.All,maqueen.Dir.CCW,movement_speed)
+    }else if (!(move) && moving) {
         maqueen.motorStop(maqueen.Motors.All)
-        turning = false
+        moving = false
         turning_left = false
         turning_right = false
     }
@@ -110,14 +124,17 @@ basic.forever(function () {
 basic.forever(function () {
     if (scan) {
         dist = maqueen.Ultrasonic()
-        turn = true
+        move = true
         turn_right = true
         if (dist <= 40) {
-            turn = false
+            move = false
             turn_right = false
             scan = false
             approach = true
         }
         basic.pause(25)
+    }else if (approach){
+        move = true
+        move_forward = true
     }
 })
